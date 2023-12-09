5300f13e7bd46e529e2c89257a48acf6b00f0cdd
Nouman945 <muhmmadnouman945@gmail.com>
2023-12-09 16:00:50 +0300

    Updates

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -4,6 +4,7 @@ import GanttChartComponent from './GanttChartComponent';
 import StackedBarChart from './StackedBarChart';
 import Card from '@/components/ui/Card';
 import TaskDistributionChart from './TaskDistributionChart';
+import MetricsComponent from '../metrics/metrices';
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -13,16 +14,16 @@ const App = () => {
     const selectedFileId = localStorage.getItem('selectedFileId');
     setSelectedFileId(selectedFileId);
     console.log(selectedFileId);
-      const fetchData = async () => {
-        // You would replace this with your actual API call
-        const response = await fetch(`https://django-apis-0a980656a9f1.herokuapp.com/tasks/?file_id=${selectedFileId}`);
-        const jsonData = await response.json();
-        console.log(jsonData);
-        setData(jsonData);
-      };
-  
-      fetchData();
-    
+    const fetchData = async () => {
+      // You would replace this with your actual API call
+      const response = await fetch(`https://django-apis-0a980656a9f1.herokuapp.com/tasks/?file_id=${selectedFileId}`);
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setData(jsonData);
+    };
+
+    fetchData();
+
   }, [selectedFileId]);
   const chartContainerStyle = {
     display: 'flex',
@@ -33,31 +34,32 @@ const App = () => {
   return (
     <>
 
-  <div className="space-y-5">
-    <div className="grid grid-cols-1 xl:grid-cols-10 gap-5">
-      {/* First component occupying 4 out of 10 columns (40%) on xl screens */}
-      <div className="xl:col-span-4 col-span-1">
-        <Card title="Task Completion Comparison">
-          <div  style={chartContainerStyle}>
-            <PieChartComponent data={data} /> 
-          </div>
-        </Card>
-      </div>
-      {/* Second component occupying 6 out of 10 columns (60%) on xl screens */}
-      <div className="xl:col-span-6 col-span-1">
-        <Card title="Task Distribution by Category">
-          <TaskDistributionChart data={data}/>
-        </Card>
-      </div>
-    </div>
-  </div>
+      <div className="space-y-5">
+        <MetricsComponent />
 
+        <div className="grid grid-cols-1 xl:grid-cols-10 gap-5">
+          {/* First component occupying 4 out of 10 columns (40%) on xl screens */}
+          <div className="xl:col-span-4 col-span-1">
+            <Card title="Task Completion Comparison">
+              <div style={chartContainerStyle}>
+                <PieChartComponent data={data} />
+              </div>
+            </Card>
+          </div>
+          {/* Second component occupying 6 out of 10 columns (60%) on xl screens */}
+          <div className="xl:col-span-6 col-span-1">
+            <Card title="Task Distribution by Category">
+              <TaskDistributionChart data={data} />
+            </Card>
+          </div>
+        </div>
+      </div>
       <br />
       <div className=" space-y-5">
-       
-          <Card title="Gannt Chart">
-            <GanttChartComponent data={data}/>
-          </Card>
+
+        <Card title="Gannt Chart">
+          <GanttChartComponent data={data} />
+        </Card>
       </div>
     </>
   );
--- src/pages/metrics/metrices.jsx
+++ src/pages/metrics/metrices.jsx
@@ -80,85 +80,83 @@ const MetricsComponent = () => {
         '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'
       ];
     }
-
     return chartData;
   };
 
 
 
   return (
-<div className="grid xl:grid-cols-3 grid-cols-1 gap-5">
+    <div className="grid xl:grid-cols-3 grid-cols-1 gap-5">
       {Object.keys(metrics).length === 0 ? (
         <p>Loading...</p>
       ) : (
         <>
-      {/* Total Bookings - Line Chart */}
+          {/* Total Bookings - Line Chart */}
+          <Card title="Total Bookings">
+            <Line options={chartOptions} data={createChartData('Total Bookings')} />
+          </Card>
 
-      <Card title="Total Bookings">
-        <Line options={chartOptions} data={createChartData('Total Bookings')} />
-      </Card>
+          {/* Average Deal Size - Line Chart */}
+          <Card title="Average Deal Size">
+            <Line options={chartOptions} data={createChartData('Average Deal Size')} />
+          </Card>
 
-      {/* Average Deal Size - Line Chart */}
-      <Card title="Average Deal Size">
-        <Line options={chartOptions} data={createChartData('Average Deal Size')} />
-      </Card>
+          {/* Average Months Paid Upfront - Bar Chart */}
+          <Card title="Average Months Paid Upfront">
+            <Bar options={chartOptions} data={createChartData('Average Months Paid Upfront', 'bar')} />
+          </Card>
 
-      {/* Average Months Paid Upfront - Bar Chart */}
-      <Card title="Average Months Paid Upfront">
-        <Bar options={chartOptions} data={createChartData('Average Months Paid Upfront', 'bar')} />
-      </Card>
+          {/* Average ARPA for New Customers - Line Chart */}
+          <Card title="Average ARPA for New Customers">
+            <Line options={chartOptions} data={createChartData('Average ARPA for New Customers')} />
+          </Card>
 
-      {/* Average ARPA for New Customers - Line Chart */}
-      <Card title="Average ARPA for New Customers">
-        <Line options={chartOptions} data={createChartData('Average ARPA for New Customers')} />
-      </Card>
+          {/* Average ARPA Across Installed Base - Line Chart */}
+          <Card title="Average ARPA Across Installed Base">
+            <Line options={chartOptions} data={createChartData('Average ARPA Across Installed Base')} />
+          </Card>
 
-      {/* Average ARPA Across Installed Base - Line Chart */}
-      <Card title="Average ARPA Across Installed Base">
-        <Line options={chartOptions} data={createChartData('Average ARPA Across Installed Base')} />
-      </Card>
+          {/* Average Gross Margin % - Pie Chart */}
+          <Card title="Average Gross Margin %">
+            <Pie options={chartOptions} data={createChartData('Average Gross Margin %', 'pie')} />
+          </Card>
 
-      {/* Average Gross Margin % - Pie Chart */}
-      <Card title="Average Gross Margin %">
-        <Pie options={chartOptions} data={createChartData('Average Gross Margin %', 'pie')} />
-      </Card>
+          {/* Average Total Expenses - Bar Chart */}
+          <Card title="Average Total Expenses">
+            <Bar options={chartOptions} data={createChartData('Average Total Expenses', 'bar')} />
+          </Card>
 
-      {/* Average Total Expenses - Bar Chart */}
-      <Card title="Average Total Expenses">
-        <Bar options={chartOptions} data={createChartData('Average Total Expenses', 'bar')} />
-      </Card>
+          {/* Average Customer Engagement Score - Line Chart */}
+          <Card title="Average Customer Engagement Score">
+            <h2></h2>
+            <Line options={chartOptions} data={createChartData('Average Customer Engagement Score')} />
+          </Card>
 
-      {/* Average Customer Engagement Score - Line Chart */}
-      <Card title="Average Customer Engagement Score">
-        <h2></h2>
-        <Line options={chartOptions} data={createChartData('Average Customer Engagement Score')} />
-      </Card>
+          {/* Average Net Promoter Score - Line Chart */}
+          <Card title="Average Net Promoter Score">
+            <Line options={chartOptions} data={createChartData('Average Net Promoter Score')} />
+          </Card>
 
-      {/* Average Net Promoter Score - Line Chart */}
-      <Card title="Average Net Promoter Score">
-        <Line options={chartOptions} data={createChartData('Average Net Promoter Score')} />
-      </Card>
+          {/* Average LTV - Line Chart */}
+          <Card title="Average LTV">
+            <Line options={chartOptions} data={createChartData('Average LTV')} />
+          </Card>
 
-      {/* Average LTV - Line Chart */}
-      <Card title="Average LTV">
-        <Line options={chartOptions} data={createChartData('Average LTV')} />
-      </Card>
+          {/* Average CAC - Bar Chart */}
+          <Card title="Average CAC">
+            <Bar options={chartOptions} data={createChartData('Average CAC', 'bar')} />
+          </Card>
 
-      {/* Average CAC - Bar Chart */}
-      <Card title="Average CAC">
-        <Bar options={chartOptions} data={createChartData('Average CAC', 'bar')} />
-      </Card>
+          {/* Average LTV to CAC Ratio - Bar Chart */}
+          <Card title="Average LTV to CAC Ratio">
+            <Bar options={chartOptions} data={createChartData('Average LTV to CAC Ratio', 'bar')} />
+          </Card>
 
-      {/* Average LTV to CAC Ratio - Bar Chart */}
-      <Card title="Average LTV to CAC Ratio">
-        <Bar options={chartOptions} data={createChartData('Average LTV to CAC Ratio', 'bar')} />
-      </Card>
-
-      {/* Average Months to Recover CAC - Line Chart */}
-      <Card title="Average Months to Recover CAC">
+          {/* Average Months to Recover CAC - Line Chart */}
+          {/* <Card title="Average Months to Recover CAC">
         <Line options={chartOptions} data={createChartData('Average Months to Recover CAC')} />
-      </Card>
-      </>
+      </Card> */}
+        </>
       )}
     </div>
   );
