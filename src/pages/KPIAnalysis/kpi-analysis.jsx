ed1d7b736c7b92b923aa2707d09c5a6c7deba548
Nouman945 <muhmmadnouman945@gmail.com>
2023-12-09 14:15:18 +0300

    Updates

--- src/pages/KPIAnalysis/kpi-analysis.jsx
+++ src/pages/KPIAnalysis/kpi-analysis.jsx
@@ -1,43 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Card, Button, Modal } from 'antd';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import moment from 'moment'; // For date formatting
 import "./customStyles.css";
 
 function ForecastModal({ isVisible, data, onClose }) {
+    // Format the data for the chart
+    const formattedData = data ? data.dates.map((date, index) => ({
+        date: date,
+        value: parseFloat(data.values[index])
+    })) : [];
+
     return (
-        <Modal
-            title="AI Forecast Data"
-            visible={isVisible}
-            onCancel={onClose}
-            footer={null}
-        >
-            <div className="forecast-container">
-                <div className="forecast-table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Date</th>
-                                <th>Value</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data && data.dates.map((date, index) => (
-                                <tr key={index}>
-                                    <td>{date}</td>
-                                    <td>{data.values[index]}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </Modal>
+        <Card>
+            <Modal
+                title="AI Forecast Data"
+                visible={isVisible}
+                onCancel={onClose}
+                footer={null}
+                width={700}
+            >
+                <br />
+                <LineChart width={600} height={300} data={formattedData}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="date" />
+                    <YAxis />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+                </LineChart>
+            </Modal>
+        </Card>
     );
 }
+
 
 function TableNames() {
     const [tables, setTables] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [isModalVisible, setIsModalVisible] = useState(false);
     const [forecastData, setForecastData] = useState(null);
     const [isForecastModalVisible, setIsForecastModalVisible] = useState(false);
 
@@ -54,25 +53,19 @@ function TableNames() {
             });
     }, []);
 
-    const showForecastModal = () => {
-        setIsModalVisible(true);
-    };
-
-    const handleModalCancel = () => {
-        setIsModalVisible(false);
-        setForecastData(null);
-    };
-
     const handleForecastModalCancel = () => {
         setIsForecastModalVisible(false);
     };
 
+
+
+
     const handleAnalyze = (record) => {
         const requestData = { 
             months: record.cols, 
-            bookings: record.rows.flat()  // Assuming record contains months and bookings
+            bookings: record.rows.flat()
         };
-
+    
         fetch('https://django-apis-0a980656a9f1.herokuapp.com/forecast-months/', {
             method: 'POST',
             headers: {
@@ -82,13 +75,26 @@ function TableNames() {
         })
         .then(response => response.json())
         .then(data => {
-            setForecastData(data.forecast);
+            console.log("forecastData", data);
+            console.log("requestData", requestData);
+    
+            // Combine the forecast and actual data
+            const combinedData = {
+                dates: [...requestData.months, ...data.dates],
+                values: [...requestData.bookings, ...data.values]
+            };
+    
+            console.log("Combined Data", combinedData);
+    
+            // Use combinedData as needed, e.g., 
+            setForecastData(combinedData);
             setIsForecastModalVisible(true);
         })
         .catch(error => {
             console.error('Error fetching forecast data:', error);
         });
     };
+    
 
     const renderTable = (tableData, tableName) => {
         let columnWidth = 100 / (tableData.cols.length + 1); // +1 for the action column
@@ -118,19 +124,6 @@ function TableNames() {
                     >
                         Analyze
                     </Button>
-                    {/* <Button 
-                        onClick={() => console.log('Edit', record)} 
-                        style={{ marginRight: 8, backgroundColor: 'blue', color: 'white' }}
-                    >
-                        Edit
-                    </Button>
-                    <Button 
-                        onClick={() => console.log('Delete', record)} 
-                        style={{ backgroundColor: 'red', color: 'white' }}
-                        danger
-                    >
-                        Delete
-                    </Button> */}
                 </div>
             ),
         });
