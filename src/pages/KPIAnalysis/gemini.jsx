f5af5a95658d48976b0cd9c2e110b7ca59d52172
Nouman945 <muhmmadnouman945@gmail.com>
2023-12-24 23:52:42 +0300

    Updates

--- src/pages/KPIAnalysis/gemini.jsx
+++ src/pages/KPIAnalysis/gemini.jsx
@@ -2,24 +2,34 @@ import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import Card from "@/components/ui/Card";
 import LoaderCircle from "@/components/Loader-circle";
+
 function ApiDisplay() {
     const [response, setResponse] = useState("");
     const [loading, setLoading] = useState(true);
+    const [refresh, setRefresh] = useState(false);
 
     useEffect(() => {
-        // Fetch data from your Django API when the component mounts
+        setLoading(true);
         fetch("https://django-apis-0a980656a9f1.herokuapp.com/generate-data/")
             .then((response) => response.json())
             .then((data) => {
                 setResponse(data.response);
                 setLoading(false);
-                console.log(data);
             })
             .catch((error) => console.error("Error fetching data:", error));
-    }, []);
+    }, [refresh]);
+
+    const handleRefresh = () => {
+        fetch("https://django-apis-0a980656a9f1.herokuapp.com/clear-data/")
+            .then(() => {
+                setRefresh(!refresh); // Toggle the refresh state to re-trigger useEffect
+            })
+            .catch((error) => console.error("Error clearing data:", error));
+    };
 
     return (
         <Card title="AI Analysis">
+            <button onClick={handleRefresh}>Refresh Data</button>
             {loading ? (
                 <LoaderCircle />
             ) : (
